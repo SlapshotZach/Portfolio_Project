c56c2d46fd558dd8ba770903432de6df82cf68ca
Zachary.Wheeler <zwheeler0211@gmail.com>
2025-04-27 13:55:44 -0500

    Adding Basics of Routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ExperiencePageComponent } from './experience-page/experience-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { ProjectsPageComponent } from './projects-page/projects-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: 'about_me', component: AboutPageComponent },
+    { path: 'experience', component: ExperiencePageComponent },
+    { path: 'projects', component: ProjectsPageComponent },
+    { path: '**', component: HomePageComponent },
+];
